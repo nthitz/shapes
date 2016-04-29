b1eda5e18c4cc3f77276e5582c1b11a1e39bd806
Nick Yahnke <nthitz@gmail.com>
2016-04-28 23:11:50 -0700

    add intersection conditions

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import _ from 'lodash'
+import lineSegmentsIntersect from 'line-segments-intersect'
 
 import style from './styles.css'
 import shapesParser from './shapes.js'
@@ -11,6 +12,7 @@ let input = document.getElementById('input')
 let output = document.getElementById('output')
 let ctx = output.getContext('2d')
 
+let shapes = []
 let color = 'white'
 
 init()
@@ -39,17 +41,90 @@ function reset() {
   let code = input.value
   let program = shapesParser.parse(code)
   console.log(program)
-  let shapes = []
+  shapes.length = 0
+  window.shapes = shapes
   ctx.clearRect(0, 0, dim, dim)
   ctx.fillStyle = color
+  ctx.strokeStyle = color
   rafId = requestAnimationFrame(draw)
+
+  let halfSize = program.size / 2
+  let twoPi = Math.PI * 2
   function draw() {
     rafId = requestAnimationFrame(draw)
     let x = Math.random() * dim
     let y = Math.random() * dim
     let angle = 0
 
-    ctx.fillRect(x, y, 10, 10)
+    assignAngle()
+
+    let pass = checkConditions()
+    if (!pass) {
+      return
+    }
+
+    ctx.save()
+    ctx.beginPath()
+    ctx.translate(x - halfSize, y - halfSize)
+    ctx.rotate(angle)
+
+
+    if (program.shape === 'square') {
+      ctx.fillRect(0, 0, program.size, program.size)
+    } else if (program.shape === 'line') {
+      ctx.moveTo(-halfSize, 0)
+      ctx.lineTo(halfSize, 0)
+      ctx.stroke()
+    } else if (program.shape === 'circle') {
+      ctx.arc(0, 0, program.size, 0, twoPi, true)
+      ctx.fill()
+    }
+    ctx.restore()
+
+    function assignAngle() {
+      if (program.angle) {
+        if (program.angle.random) {
+          angle = Math.random() * twoPi
+        }
+      }
+    }
+
+    function checkConditions() {
+      if (! program.condition) return true
+      if (typeof program.condition.intersect === 'undefined') return true
+      let shape = null;
+      if (program.shape === 'line') {
+        shape = [
+          [x - Math.cos(angle) * halfSize, y - Math.sin(angle) * halfSize],
+          [x + Math.cos(angle) * halfSize, y + Math.sin(angle) * halfSize]
+        ]
+      }
+
+      if (shapes.length === 0) return addShapeAndReturn()
+
+      if (program.shape === 'line') {
+        let hitsNone = true
+        for (let i = 0; i < shapes.length; i++) {
+          let testShape = shapes[i]
+          if (lineSegmentsIntersect(shape, testShape)) {
+            hitsNone = false
+            if (program.condition.intersect) {
+              return addShapeAndReturn()
+            }
+          }
+        }
+        if (! program.condition.intersect && hitsNone) {
+          return addShapeAndReturn()
+        }
+        return false
+      }
+
+      function addShapeAndReturn() {
+        shapes.push(shape)
+        // console.log(shape)
+        return true
+      }
+    }
   }
 }
 
